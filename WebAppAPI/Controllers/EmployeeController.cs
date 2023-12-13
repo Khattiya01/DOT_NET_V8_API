c42bd7b9756c8800e3652fbd5aa7bce347f313e6
Khattiya01 <khattiya.dokbua@gmail.com>
2023-12-13 14:56:07 +0700

    3

--- WebAppAPI/Controllers/EmployeeController.cs
+++ WebAppAPI/Controllers/EmployeeController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebAppAPI.Data;
+using WebAppAPI.Models.Repositorys;
+using WebAppAPI.Models;
 
 namespace WebAppAPI.Controllers
 {
@@ -16,13 +18,32 @@ namespace WebAppAPI.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult> GetAllShirts()
+        public async Task<ActionResult> GetAllEmployee()
         {
             var datas = await _context.Shirt.ToListAsync();
             return Ok(datas);
             /* return Ok(new { data = ShirtRepository.GetShirts(),
              meta = "meta"
              });*/
+        }
+
+        [HttpGet("id")]
+        public async Task<ActionResult> GetEmployeeById(int id)
+        {
+
+            var employee = await _context.Employees.FindAsync(id);
+            if (employee is null)
+            {
+                return NotFound();
+            }
+            return Ok(employee);
+        }
+
+        [HttpPost]
+        public IActionResult CreateEmployee([FromBody] Employee employee)
+        {
+            _context.Employees.Add(employee);
+            return Ok();
         }
     }
 }
